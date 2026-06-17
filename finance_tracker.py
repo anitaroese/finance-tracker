30509af6f12bd5c58713f68ad664eab34e22d4a8
Anita Roese <anitatrevisan@gmail.com>
2026-06-17 13:04:04 -0300

    feat: add category selection for expense transactions

--- finance_tracker.py
+++ finance_tracker.py
@@ -19,7 +19,7 @@ def save_data(transactions):
 def export_to_csv(transactions):
     """Export existed data into finance_data.csv file"""
     with open("finance_data.csv", "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=["type", "name", "amount", "date"])
+        writer = csv.DictWriter(file, fieldnames=["type", "name", "amount", "date", "category"])
         writer.writeheader()
         for transaction in transactions:
             writer.writerow(transaction)
@@ -35,39 +35,71 @@ def get_number(prompt):
 
 def get_transaction(transaction_type):
     """Prompt the user for a transaction, storing the name, amount, type and date"""
+    if transaction_type == "expense":
+        while True:
+            category = input("\nCategories: \n1. Groceries \n2. Dining out \n3. Subscriptions \n4. Entertainment \n5. Clothing \n6. Gifts \n7. Other \nChoose an option: ")
+
+            if category == "1":
+                transaction_category = "groceries"
+                break
+            elif category == "2":
+                transaction_category = "dining out"
+                break
+            elif category == "3":
+                transaction_category = "subscriptions"
+                break
+            elif category == "4":
+                transaction_category = "entertainment"
+                break
+            elif category == "5":
+                transaction_category = "clothing"
+                break
+            elif category == "6":
+                transaction_category = "gifts"
+                break
+            elif category == "7":
+                transaction_category = "other"
+                break
+            else:
+                print("Invalid choice")
+            
+    
     while True: 
         name = input("Enter transaction name: ").strip().lower()
         
         if name == "":
             print("Transaction name cannot be empty.")
-            continue
-
-        while True: 
-            amount = get_number(f"Enter amount for {name}: $")
-
-            if amount <= 0:
-                print("Amount must be greater than 0.")
-                continue
-
+        else:
             break
 
-        while True:
-            choice = input("Date (press Enter for today, or type in YYYY-MM-DD format):")
-            if choice == "":
-                date = datetime.now().strftime("%Y-%m-%d")
+    while True: 
+        amount = get_number(f"Enter amount for {name}: $")
+
+        if amount <= 0:
+            print("Amount must be greater than 0.")
+        else:
+            break
+
+    while True:
+        choice = input("Date (press Enter for today, or type in YYYY-MM-DD format):")
+        if choice == "":
+            date = datetime.now().strftime("%Y-%m-%d")
+            break
+        else:
+            try:
+                datetime.strptime(choice.strip(), "%Y-%m-%d") # check if user's input is valid by trying to convert it to datetime format
+                date = choice
                 break
-            else:
-                try:
-                    datetime.strptime(choice.strip(), "%Y-%m-%d") # check if user's input is valid by trying to convert it to datetime format
-                    date = choice
-                    break
-                except ValueError:
-                    print("Invalid format")
+            except ValueError:
+                print("Invalid format")
 
-        
-        transaction = {"type":transaction_type, "name":name, "amount":amount, "date":date}
+    
+    if transaction_type == "expense":
+        transaction = {"type":transaction_type, "category": transaction_category, "name":name, "amount":amount, "date":date}
+    else:
+        transaction = {"type":transaction_type, "category":"", "name":name, "amount":amount, "date":date}
 
-        return transaction
+    return transaction
 
 def show_summary(transactions):
     """Show summary of transactions including total expenses, total incomes, and balance"""
